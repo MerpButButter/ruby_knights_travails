58d0f5c5a2e01e1742afa73bc12fe31c877a240f
marc <marc.levyon@gmail.com>
2023-01-21 10:05:46 +0200

    Shortest Path Through Binary Tree V3 This one finally works. It just returns the node which gets there first. Don't know whats much different from the previous iterations, but all is well that ends well.

--- main.rb
+++ main.rb
@@ -7,12 +7,24 @@ def knights_travails(start, goal)
   board = Board.new(knight)
   shortest_path = Tree.new(knight.position).shortest_path(goal, Knight::MOVES, board.method(:move_piece))
   
-  shortest_path.each do |position|
+
+  
+  shortest_path_array(shortest_path).each do |position|
     sleep(1)
     board.place_piece(position.map { |n| n - 1 })
     board.display
   end
-  puts("It took #{shortest_path.length} moves.".color("orange"))
+  puts("It took #{shortest_path.distance} moves.".color("orange"))
 end
 
-knights_travails([4, 4], [6, 4])
+def shortest_path_array(node)
+  short_path = []
+  until node.nil?
+    short_path.unshift(node.data)
+    node = node.parent
+  end
+  short_path.shift
+  short_path
+end
+
+knights_travails([1, 1], [8, 8])
--- node_tree.rb
+++ node_tree.rb
@@ -4,68 +4,37 @@ class Tree
   attr_reader :root
   
   class Node 
-    attr_accessor :data, :parent
+    attr_accessor :data, :parent, :distance
   
-    def initialize(data, parent = nil)
+    def initialize(data, distance , parent = nil)
       @data = data
       @parent = parent
+      @distance = distance
     end
   end
 
   def initialize(root)
-    @root = Node.new(root)
-  end
-
-  def level_order
-    @queue.push(*yield(@queue.shift)).compact! until @queue.empty?
+    @root = Node.new(root, 0)
   end
 
   def shortest_path(goal, moves, method)
-    shortest_path_length = 1000
-    made_moves = [@root.data]
-    shortest_paths = []
     queue = [@root]
+    visited = [@root.data.map { |n| n - 1 }]
 
-    until queue.empty? 
+    until queue.empty?
       node = queue.shift
-      next if shortest_path_length < move_length(node) 
       
-      if node.data == goal
-        shortest_path_length = move_length(node) if shortest_path_length > move_length(node)
-        shortest_paths.push(node)
-        next
-      else
-        moves.values.map do |move| 
-          move = method.call(move, node.data)
-          next if move.nil?
-          
-          p(queue.map {|q_node| q_node.data})
-          move.map! { |n| n + 1 }
-          next if made_moves.include?(move)
+      return node if node.data == goal
 
-          made_moves.push(move)
+      moves.each do |move|
+        location = method.call(move[1], node.data)
 
-          node = Node.new(move, node) 
-          if node.data == goal
-            shortest_path_length = move_length(node) if shortest_path_length > move_length(node)
-            shortest_paths.push(node)
-            next  
-          else
-            queue.push(node)
-          end    
+        if !location.nil? && !visited.include?(location)
+          visited << location
+          queue.push(Node.new(location.map { |n| n + 1 }, node.distance + 1, node))
         end
       end
     end
-    p shortest_paths
-  end
-
-  def move_length(node)
-    length = 0
-    until node.nil?
-      length += 1
-      node = node.parent
-    end
-    length
   end
 
 end
